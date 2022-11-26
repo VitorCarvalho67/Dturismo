3d065a31d24027ca9b24119ba1cc7f85dd40969b
Carvalho <102667323+VitorCarvalho67@users.noreply.github.com>
2022-11-25 23:18:38 -0300

    Add files via upload

--- Projeto PW - Turismo/index/js/script.js
+++ Projeto PW - Turismo/index/js/script.js
@@ -1,36 +1,74 @@
-volt = document.getElementById("reset");
-next = document.getElementById("next");
+blb = document.getElementById("btnl");
+BTN = document.getElementById("btn1");
+bl = document.getElementById("anside");
 
-next.addEventListener("click", () =>{
-
-    if(main.classList.contains("main-1")){
-        main.classList.add("main-2");
-        main.classList.remove("main-1");
+blb.addEventListener("click", () =>{
+    
+    if(anside.classList.contains("fechado")){
+        anside.classList.add("aberto");
+        anside.classList.remove("fechado");
     }
-    else if(main.classList.contains("main-2")){
-        main.classList.add("main-3");
-        main.classList.remove("main-2");
+    else if(anside.classList.contains("aberto")){
+        anside.classList.add("fechado");
+        anside.classList.remove("aberto");
+    }
+})
+
+
+BTN.addEventListener("click", () =>{
+
+Nome = document.getElementById("Nome").value;
+Email = document.getElementById("email").value;
+
+    if(Nome == "" || Email ==""){
+        alert("Prencha os campos");
     }
     else{
-        main.classList.add("main-1");
-        main.classList.remove("main-3");
-        }
+        alert("Obrigado");
+        document.location.href = email + encodeURIComponent(Nome)+ "&body=" + encodeURIComponent("Promoção!!!!!!");
+        document.getElementById("Nome").value="";
+        document.getElementById("email").value="";
     }
-)
+})
 
-volt.addEventListener("click", () =>{
+function inicializar() {
+    var coordenadas = {lat: -22.912869, lng: -43.228963};
 
-    if(main.classList.contains("main-1")){
-        main.classList.add("main-3");
-        main.classList.remove("main-1");
-    } 
-    else if(main.classList.contains("main-3")){
-        main.classList.add("main-2");
-        main.classList.remove("main-3");
+    var mapa = new google.maps.Map(document.getElementById('mapa'), {
+      zoom: 15,
+      center: coordenadas 
+    });
+
+    var marker = new google.maps.Marker({
+      position: coordenadas,
+      map: mapa,
+      title: 'Meu marcador'
+    });
+}
+
+var pesquisa = document.getElementById("inp").value;
+pb = document.getElementById("pbtn");
+
+pb.addEventListener("click", () =>{
+    
+    
+    if(pesquisa.indexOf("lugares")){
+        console.log("oi");
+        href(/lugares/lugares.html);
     }
-    else{
-        main.classList.add("main-1");
-        main.classList.remove("main-2");
-        }
+    if(pesquisa.contains("pacotes")){
+        ../pacotes/pacotes.html
     }
-)
+    if(pesquisa.contains("preço")){
+        ../preço/preço.html
+    }
+    if(pesquisa.contains("mapa")){
+        ../index/index.html/mapa
+    }
+    if(pesquisa.contains("lugares")){
+        ../index/index.html/cupons
+    }
+
+})
+    
+
